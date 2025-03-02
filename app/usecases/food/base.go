ce563f28a7d40e1c625cf984f7395b0b7dd2b05d
Ranggalawe Istifajar <ranggalawe.istifajar@wartek.belajar.id>
2025-03-02 16:47:20 +0700

    Add DB operation and usecase

--- app/usecases/food/base.go
+++ app/usecases/food/base.go
@@ -10,6 +10,7 @@ type Usecases interface {
 	CreateFood(ctx context.Context, arg *CreateFoodParams) error
 	ListFood(ctx context.Context, arg *ListFoodParams) ([]repository.ListFoodRow, error)
 	GetFood(ctx context.Context, id int32) (repository.GetFoodRow, error)
+	UpdateFood(ctx context.Context, arg *UpdateFoodParams) error
 }
 
 type usecase struct {
