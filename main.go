685ff5d46613686ab2d578d3125537d8346c3e0f
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-03-20 20:52:01 +0700

    Move config to commons directory

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 	apiauthv1 "github.com/gatsu420/mary/api/gen/go/auth/v1"
 	apifoodv1 "github.com/gatsu420/mary/api/gen/go/food/v1"
-	"github.com/gatsu420/mary/app/config"
 	"github.com/gatsu420/mary/app/handlers"
 	"github.com/gatsu420/mary/app/interceptors"
 	"github.com/gatsu420/mary/app/usecases/auth"
 	"github.com/gatsu420/mary/app/usecases/food"
+	"github.com/gatsu420/mary/common/config"
 	"github.com/gatsu420/mary/db/repository"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
