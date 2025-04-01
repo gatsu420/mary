c29147c63e8ce8abd92bb6b7aebfe3fb5a8b205b
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-04-01 20:55:42 +0700

    Restructure auth usecase into several files

---
+++ app/usecases/auth/validate_token.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"github.com/gatsu420/mary/common/errors"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func (u *usecase) ValidateToken(signedToken string) (string, error) {
+	token, err := jwt.Parse(signedToken, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != "HS256" {
+			return nil, errors.New(errors.AuthError, "expected signing method HS256")
+		}
+
+		return []byte(u.secret), nil
+	})
+	if err != nil {
+		return "", errors.New(errors.AuthError, "invalid token")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userID, ok := claims["sub"].(string)
+		if !ok {
+			return "", errors.New(errors.AuthError, "failed to extract userID from claims")
+		}
+
+		return userID, nil
+	}
+
+	return "", errors.New(errors.AuthError, "invalid token")
+}
