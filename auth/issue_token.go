ad8305b0dbfdb5227c83c2f1214770744642dd0d
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-04-04 11:28:23 +0700

    Add unit test to auth

--- auth/issue_token.go
+++ auth/issue_token.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"time"
 
-	"github.com/gatsu420/mary/common/errors"
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -14,11 +13,7 @@ func (a *authImpl) IssueToken(username string) (string, error) {
 		"exp": time.Now().Add(15 * time.Minute).Unix(),
 	})
 
-	signedToken, err := token.SignedString([]byte(a.secret))
-	if err != nil {
-		// TODO: pass err to new error in the same fashion as this:
-		// return "", fmt.Errorf("unable to sign JWT: %w", err)
-		return "", errors.New(errors.AuthError, "unable to sign JWT")
-	}
+	// SignedString will still return token eventhouggh secret is empty string.
+	signedToken, _ := token.SignedString([]byte(a.secret))
 	return signedToken, nil
 }
