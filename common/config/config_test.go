7cad742e9af67904a7de636391d053858c47b644
Ranggalawe Istifajar <51109081+gatsu420@users.noreply.github.com>
2025-04-06 22:37:11 +0700

    Add unit test to config loading (#41)
    * Add unit test to config loading
    
    * Fix typo

---
+++ common/config/config_test.go
@@ -0,0 +1,44 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/gatsu420/mary/common/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LoadConfig(t *testing.T) {
+	testCases := []struct {
+		testName       string
+		filePath       string
+		expectedConfig *config.Config
+		expectedErr    error
+	}{
+		{
+			testName:       "file is not found",
+			filePath:       ".there.is.no.file.env",
+			expectedConfig: nil,
+		},
+		{
+			testName: "file is found",
+			filePath: "../../.env.example",
+			expectedConfig: &config.Config{
+				PostgresURL:    "postgres://user:pass@localhost:5432/testdb?sslmode=disable",
+				GRPCServerPort: "9090",
+				JWTSecret:      "secret",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			cfg, err := config.LoadConfig(tc.filePath)
+			assert.Equal(t, tc.expectedConfig, cfg)
+			if tc.expectedConfig == nil {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
