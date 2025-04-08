399f347742523d6c9ee6c50e6360fdeaada49aa9
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-04-08 21:55:37 +0700

    Add more unit tests

--- common/errors/errors_test.go
+++ common/errors/errors_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gatsu420/mary/common/errors"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
 )
 
 func Test_New(t *testing.T) {
@@ -17,5 +18,23 @@ func Test_New(t *testing.T) {
 	t.Run("successfully generate new error", func(t *testing.T) {
 		err := errors.New(category, msg)
 		assert.Equal(t, expectedErr, err)
+	})
+}
+
+func Test_Error(t *testing.T) {
+	mockErr := errors.New(errors.AuthError, "auth error")
+	expectedErrMsg := "auth error"
+	t.Run("successfully get error message", func(t *testing.T) {
+		errMsg := mockErr.Error()
+		assert.Equal(t, expectedErrMsg, errMsg)
+	})
+}
+
+func Test_GRPCCode(t *testing.T) {
+	mockErr := errors.New(errors.AuthError, "auth error")
+	expectedGRPCCode := codes.Unauthenticated
+	t.Run("successfully get GRPC code", func(t *testing.T) {
+		grpcCode := mockErr.GRPCCode()
+		assert.Equal(t, expectedGRPCCode, grpcCode)
 	})
 }
