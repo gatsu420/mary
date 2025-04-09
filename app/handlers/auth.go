675eb7a65476ff3925b12cfa2e9f06f47d7d4006
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-04-09 22:06:12 +0700

    Move constructor and test suite in separate file

--- app/handlers/auth.go
+++ app/handlers/auth.go
@@ -4,22 +4,7 @@ import (
 	"context"
 
 	apiauthv1 "github.com/gatsu420/mary/api/gen/go/auth/v1"
-	"github.com/gatsu420/mary/app/usecases/users"
-	"github.com/gatsu420/mary/auth"
 )
-
-type AuthServer struct {
-	apiauthv1.UnimplementedAuthServiceServer
-	Auth         auth.Auth
-	UsersUsecase users.Usecase
-}
-
-func NewAuthServer(auth auth.Auth, usersUsecase users.Usecase) *AuthServer {
-	return &AuthServer{
-		Auth:         auth,
-		UsersUsecase: usersUsecase,
-	}
-}
 
 func (s *AuthServer) IssueToken(ctx context.Context, user *apiauthv1.IssueTokenRequest) (*apiauthv1.IssueTokenResponse, error) {
 	if err := s.UsersUsecase.CheckUserIsExisting(ctx, user.Username); err != nil {
---
+++ app/handlers/base_test.go
@@ -0,0 +1,59 @@
+package handlers_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/gatsu420/mary/app/handlers"
+	mockfood "github.com/gatsu420/mary/mocks/app/usecases/food"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+type testSuite struct {
+	suite.Suite
+	mockUsecase *mockfood.MockUsecase
+	server      handlers.FoodServer
+
+	stubCtx      context.Context
+	stubTimeSec  int
+	stubTimeNano int
+
+	stubStringWrapper *wrapperspb.StringValue
+	stubString        string
+	stubPGText        pgtype.Text
+
+	stubTimestampWrapper *timestamppb.Timestamp
+	stubPGTimestamptz    pgtype.Timestamptz
+}
+
+func (s *testSuite) SetupSuite() {
+	s.stubCtx = context.Background()
+	s.stubTimeSec = 1744208782
+	s.stubTimeNano = 99
+
+	s.stubStringWrapper = &wrapperspb.StringValue{Value: "test"}
+	s.stubString = "test"
+	s.stubPGText = pgtype.Text{String: "test", Valid: true}
+
+	s.stubTimestampWrapper = &timestamppb.Timestamp{
+		Seconds: int64(s.stubTimeSec),
+		Nanos:   int32(s.stubTimeNano),
+	}
+	s.stubPGTimestamptz = pgtype.Timestamptz{
+		Time:  time.Unix(int64(s.stubTimeSec), int64(s.stubTimeNano)),
+		Valid: true,
+	}
+}
+
+func (s *testSuite) SetupTest() {
+	s.mockUsecase = mockfood.NewMockUsecase(s.T())
+	s.server = *handlers.NewFoodServer(s.mockUsecase)
+}
+
+func Test(t *testing.T) {
+	suite.Run(t, &testSuite{})
+}
