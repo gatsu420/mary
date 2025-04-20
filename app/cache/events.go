d8dc2a51c0763d65029d8c82e7a363992fbb964c
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-04-20 21:56:11 +0700

    Change cached event structure from hash to list

--- app/cache/events.go
+++ app/cache/events.go
@@ -14,10 +14,11 @@ type CreateEventParams struct {
 
 func (s *Store) CreateEvent(ctx context.Context, arg CreateEventParams) error {
 	userCtx := ctxvalue.GetUser(ctx)
+	listKey := fmt.Sprintf("%v:%v", userCtx.UserID, arg.Name)
 	currentTime := fmt.Sprintf("%v", time.Now().Unix())
 
-	cmd := s.valkeyClient.B().Hset().Key(userCtx.UserID).FieldValue().
-		FieldValue(currentTime, arg.Name).
+	cmd := s.valkeyClient.B().Lpush().Key(listKey).
+		Element(currentTime).
 		Build()
 	if err := s.valkeyClient.Do(ctx, cmd).Error(); err != nil {
 		return err
