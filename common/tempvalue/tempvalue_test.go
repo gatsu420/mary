f4246d761cde5fe3b23e5a24acfd37fab32717cc
Ranggalawe Istifajar <51109081+gatsu420@users.noreply.github.com>
2025-04-27 12:33:59 +0700

    feat: add tool to hold variable outside ctx (#76)
    * add tool to hold variable outside ctx
    
    * change test case name

---
+++ common/tempvalue/tempvalue_test.go
@@ -0,0 +1,19 @@
+package tempvalue_test
+
+import (
+	"testing"
+
+	"github.com/gatsu420/mary/common/tempvalue"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_SetAndGetUserID(t *testing.T) {
+	t.Run("set and get userID successfully", func(t *testing.T) {
+		fakeValue := tempvalue.NewValue()
+		fakeUserID := "fakeUserID"
+
+		fakeValue.SetUserID(fakeUserID)
+		userID := fakeValue.GetUserID()
+		assert.Equal(t, fakeUserID, userID)
+	})
+}
