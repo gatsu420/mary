d351437a2d36eeda8d808e3b7ac87a966aea2ea0
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-04-30 22:58:47 +0700

    add unit test

--- common/tempvalue/tempvalue_test.go
+++ common/tempvalue/tempvalue_test.go
@@ -16,3 +16,32 @@ func Test_SetAndGetUserID(t *testing.T) {
 		assert.Equal(t, fakeUserID, userID)
 	})
 }
+
+func Test_SetAndGetCalledMethods(t *testing.T) {
+	t.Run("set and get calledMethods successfully", func(t *testing.T) {
+		fakeMethod := "fake method"
+		tempvalue.SetCalledMethods(fakeMethod)
+		dummyMethod := "dummy method"
+		tempvalue.SetCalledMethods(dummyMethod)
+
+		methods := tempvalue.GetCalledMethods()
+		_, ok := methods[fakeMethod]
+		assert.True(t, ok)
+		_, ok = methods[dummyMethod]
+		assert.True(t, ok)
+		_, ok = methods["non existent method"]
+		assert.False(t, ok)
+	})
+}
+
+func Test_SetAndFlushCalledMethods(t *testing.T) {
+	t.Run("set and flush calledMethods successfully", func(t *testing.T) {
+		fakeMethod := "fake method"
+		tempvalue.SetCalledMethods(fakeMethod)
+		dummyMethod := "dummy method"
+		tempvalue.SetCalledMethods(dummyMethod)
+
+		tempvalue.FlushCalledMethods()
+		assert.Equal(t, 0, len(tempvalue.GetCalledMethods()))
+	})
+}
