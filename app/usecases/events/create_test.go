8c51c1bfa87ef725d96500ac8861af9ea542a038
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-05-01 17:41:27 +0700

    test: add more test cases

--- app/usecases/events/create_test.go
+++ app/usecases/events/create_test.go
@@ -50,6 +50,52 @@ func (s *testSuite) Test_CreateEvent() {
 			repoCreateEventErr: errors.New(errors.InternalServerError, "repo error"),
 			expectedErr:        errors.New(errors.InternalServerError, "DB failed to create event"),
 		},
+		{
+			testName: "cache failed when deleting events",
+			arg: &events.CreateEventParams{
+				Name: map[string]struct{}{
+					"GetFood": {},
+				},
+			},
+			cacheEvents: []cache.GetEventResponse{
+				{
+					Name:   "GetFood",
+					UserID: "test_user",
+					CreatedAtEpoch: []int64{
+						1746075368,
+						1746075377,
+					},
+				},
+			},
+			cacheGetEventsErr:    nil,
+			repoEventRownum:      0,
+			repoCreateEventErr:   nil,
+			cacheDeleteEventsErr: errors.New(errors.InternalServerError, "cache error"),
+			expectedErr:          errors.New(errors.InternalServerError, "cache failed to delete event"),
+		},
+		{
+			testName: "success",
+			arg: &events.CreateEventParams{
+				Name: map[string]struct{}{
+					"GetFood": {},
+				},
+			},
+			cacheEvents: []cache.GetEventResponse{
+				{
+					Name:   "GetFood",
+					UserID: "test_user",
+					CreatedAtEpoch: []int64{
+						1746075368,
+						1746075377,
+					},
+				},
+			},
+			cacheGetEventsErr:    nil,
+			repoEventRownum:      0,
+			repoCreateEventErr:   nil,
+			cacheDeleteEventsErr: nil,
+			expectedErr:          nil,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -64,6 +110,13 @@ func (s *testSuite) Test_CreateEvent() {
 					mock.Anything,
 					mock.AnythingOfType("[]repository.CreateEventParams"),
 				).Return(tc.repoEventRownum, tc.repoCreateEventErr).Once()
+			}
+
+			if tc.cacheGetEventsErr == nil && tc.repoCreateEventErr == nil {
+				s.mockCache.EXPECT().DeleteEvents(
+					mock.Anything,
+					mock.AnythingOfType("cache.DeleteEventParams"),
+				).Return(tc.cacheDeleteEventsErr).Once()
 			}
 
 			err := s.usecase.CreateEvent(s.ctx, tc.arg)
