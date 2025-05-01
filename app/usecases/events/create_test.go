f32274c0abb85b5c101b2a28b16464f3f28e43f6
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-05-01 13:36:58 +0700

    test: add unit test

---
+++ app/usecases/events/create_test.go
@@ -0,0 +1,73 @@
+package events_test
+
+import (
+	"github.com/gatsu420/mary/app/cache"
+	"github.com/gatsu420/mary/app/usecases/events"
+	"github.com/gatsu420/mary/common/errors"
+	"github.com/stretchr/testify/mock"
+)
+
+func (s *testSuite) Test_CreateEvent() {
+	testCases := []struct {
+		testName             string
+		arg                  *events.CreateEventParams
+		cacheEvents          []cache.GetEventResponse
+		cacheGetEventsErr    error
+		repoEventRownum      int64
+		repoCreateEventErr   error
+		cacheDeleteEventsErr error
+		expectedErr          error
+	}{
+		{
+			testName: "cache error when getting events",
+			arg: &events.CreateEventParams{
+				Name: map[string]struct{}{
+					"GetFood": {},
+				},
+			},
+			cacheEvents:       nil,
+			cacheGetEventsErr: errors.New(errors.InternalServerError, "cache error"),
+			expectedErr:       errors.New(errors.InternalServerError, "cache failed to get events"),
+		},
+		{
+			testName: "repo failed when creating event",
+			arg: &events.CreateEventParams{
+				Name: map[string]struct{}{
+					"GetFood": {},
+				},
+			},
+			cacheEvents: []cache.GetEventResponse{
+				{
+					Name:   "GetFood",
+					UserID: "test_user",
+					CreatedAtEpoch: []int64{
+						1746075368,
+						1746075377,
+					},
+				},
+			},
+			cacheGetEventsErr:  nil,
+			repoCreateEventErr: errors.New(errors.InternalServerError, "repo error"),
+			expectedErr:        errors.New(errors.InternalServerError, "DB failed to create event"),
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.testName, func() {
+			s.mockCache.EXPECT().GetEvents(
+				mock.Anything,
+				mock.AnythingOfType("cache.GetEventParams"),
+			).Return(tc.cacheEvents, tc.cacheGetEventsErr).Once()
+
+			if tc.cacheGetEventsErr == nil {
+				s.mockRepo.EXPECT().CreateEvent(
+					mock.Anything,
+					mock.AnythingOfType("[]repository.CreateEventParams"),
+				).Return(tc.repoEventRownum, tc.repoCreateEventErr).Once()
+			}
+
+			err := s.usecase.CreateEvent(s.ctx, tc.arg)
+			s.Equal(tc.expectedErr, err)
+		})
+	}
+}
