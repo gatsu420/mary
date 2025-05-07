b5e139437663b4fa2e94bab0d391210c68d2287d
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-05-07 21:23:08 +0700

    test: add unit test

--- app/workers/events_test.go
+++ app/workers/events_test.go
@@ -28,7 +28,7 @@ func (s *testSuite) Test_Create() {
 
 	for _, tc := range testCases {
 		s.Run(tc.testName, func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 			defer cancel()
 
 			if tc.withSetCalledMethods {
@@ -40,15 +40,9 @@ func (s *testSuite) Test_Create() {
 				).Return(nil).Once()
 			}
 
-			// If we run Create as ordinary function, unit test need to wait for loop to exit
-			// according to timeout. However, we can spawn it into another goroutine and both
-			// asserts can kick in in the first loop due to they being not concurrent-safe.
-			//
-			// 11 second lag is added because:
-			// 	1. 10 second for first loop (and will be caught by asserts)
-			// 	2. 1 second for goroutine overhead
-			go s.worker.Create(ctx)
-			time.Sleep(11 * time.Second)
+			ticker := make(chan time.Time)
+			go s.worker.Create(ctx, ticker)
+			ticker <- time.Now()
 
 			if tc.withSetCalledMethods {
 				s.mockUsecase.AssertCalled(s.T(), "CreateEvent",
