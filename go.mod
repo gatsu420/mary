d62b758987624b3b0b423ba110fc820c4765b945
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-05-10 15:06:34 +0700

    feat: add CLI capability

--- go.mod
+++ go.mod
@@ -10,12 +10,14 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.9.0
+	github.com/urfave/cli/v2 v2.27.6
 	github.com/valkey-io/valkey-go v1.0.57
 	google.golang.org/grpc v1.70.0
 	google.golang.org/protobuf v1.36.5
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -24,7 +26,9 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
--- main.go
+++ main.go
@@ -1,76 +1,22 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net"
-	"time"
+	"os"
 
-	apiauthv1 "github.com/gatsu420/mary/api/gen/go/auth/v1"
-	apifoodv1 "github.com/gatsu420/mary/api/gen/go/food/v1"
-	"github.com/gatsu420/mary/app/auth"
-	"github.com/gatsu420/mary/app/cache"
-	"github.com/gatsu420/mary/app/handlers"
-	"github.com/gatsu420/mary/app/interceptors"
-	"github.com/gatsu420/mary/app/repository"
-	"github.com/gatsu420/mary/app/usecases/events"
-	"github.com/gatsu420/mary/app/usecases/food"
-	"github.com/gatsu420/mary/app/usecases/users"
-	"github.com/gatsu420/mary/app/workers"
-	"github.com/gatsu420/mary/common/config"
-	"github.com/gatsu420/mary/dependency/pgdep"
-	"github.com/gatsu420/mary/dependency/valkeydep"
+	"github.com/gatsu420/mary/cmd"
 	"github.com/rs/zerolog/log"
-	"google.golang.org/grpc"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	cfg, err := config.New(".env")
-	if err != nil {
-		log.Fatal().Msgf("failed to read config file: %v", err)
+	app := &cli.App{
+		Name: "mary",
+		Commands: []*cli.Command{
+			cmd.ServeCmd,
+		},
 	}
 
-	dbPool, err := pgdep.NewPool(cfg.PostgresDSN)
-	if err != nil {
-		log.Fatal().Msgf("failed to create DB connection: %v", err)
-	}
-	defer dbPool.Close()
-	dbQuerier := repository.New(dbPool)
-
-	valkeyClient, err := valkeydep.New(cfg.CacheAddress)
-	if err != nil {
-		log.Fatal().Msgf("failed to create cache connection: %v", err)
-	}
-	defer valkeyClient.Close()
-	cacheStorer := cache.New(valkeyClient)
-
-	auth := auth.NewAuth(cfg, dbQuerier)
-
-	foodUsecase := food.NewUsecase(dbQuerier, cacheStorer)
-	usersUsecase := users.NewUsecase(dbQuerier)
-	eventsUsecase := events.NewUsecase(dbQuerier, cacheStorer)
-
-	grpcServer := grpc.NewServer(
-		grpc.ChainUnaryInterceptor(
-			// Should panic recoverer be put first or last?
-			interceptors.RecoverPanic(),
-			interceptors.ResponseError(),
-			interceptors.ValidateToken(auth),
-		),
-	)
-	apiauthv1.RegisterAuthServiceServer(grpcServer, handlers.NewAuthServer(auth, usersUsecase))
-	apifoodv1.RegisterFoodServiceServer(grpcServer, handlers.NewFoodServer(foodUsecase))
-
-	worker := workers.New(eventsUsecase)
-	workerCtx := context.Background()
-	workerTicker := time.NewTicker(10 * time.Second)
-	defer workerTicker.Stop()
-	go worker.Create(workerCtx, workerTicker.C)
-
-	port := fmt.Sprintf(":%v", cfg.GRPCServerPort)
-	listener, _ := net.Listen("tcp", port)
-	log.Info().Msgf("starting gRPC server at port %v", port)
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatal().Msg("gRPC server failed to start")
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal().Msg(err.Error())
 	}
 }
