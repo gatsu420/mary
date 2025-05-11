4444fb55a6ff5887968c5e94ffbe4a007c66a8b2
Ranggalawe Istifajar <istifajar.he@gmail.com>
2025-05-11 12:22:38 +0700

    test: update mock

--- mocks/app/usecases/authn/mock_usecase.go
+++ mocks/app/usecases/authn/mock_usecase.go
@@ -67,6 +67,64 @@ func (_c *MockUsecase_CreateMembershipRegistry_Call) RunAndReturn(run func(conte
 	return _c
 }
 
+// GetMembershipRegistry provides a mock function with given fields: ctx
+func (_m *MockUsecase) GetMembershipRegistry(ctx context.Context) ([]string, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetMembershipRegistry")
+	}
+
+	var r0 []string
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) ([]string, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) []string); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]string)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockUsecase_GetMembershipRegistry_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetMembershipRegistry'
+type MockUsecase_GetMembershipRegistry_Call struct {
+	*mock.Call
+}
+
+// GetMembershipRegistry is a helper method to define mock.On call
+//   - ctx context.Context
+func (_e *MockUsecase_Expecter) GetMembershipRegistry(ctx interface{}) *MockUsecase_GetMembershipRegistry_Call {
+	return &MockUsecase_GetMembershipRegistry_Call{Call: _e.mock.On("GetMembershipRegistry", ctx)}
+}
+
+func (_c *MockUsecase_GetMembershipRegistry_Call) Run(run func(ctx context.Context)) *MockUsecase_GetMembershipRegistry_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context))
+	})
+	return _c
+}
+
+func (_c *MockUsecase_GetMembershipRegistry_Call) Return(_a0 []string, _a1 error) *MockUsecase_GetMembershipRegistry_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *MockUsecase_GetMembershipRegistry_Call) RunAndReturn(run func(context.Context) ([]string, error)) *MockUsecase_GetMembershipRegistry_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewMockUsecase creates a new instance of MockUsecase. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewMockUsecase(t interface {
